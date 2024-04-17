2a0ac496259883255694af3b66658f9642ae9c57
Keerthikolatis <165782826+Keerthikolatis@users.noreply.github.com>
2024-04-17 17:39:57 +0530

    Update codescene-entrypoint.sh

--- scripts/codescene-entrypoint.sh
+++ scripts/codescene-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 # Run CodeScene analysis
-codescene analyze --config /app/codescene.yaml --token $CODESCENE_API_TOKEN
+codescene analyze --config /app/codescene.yaml --token S_NYpqdEUimo6yice90UhGYt4NmOr4CvxZ2fULoFUpk.C3oeAHoGjisKbrzBzbG3pZve1aXZkDBRQafvftjpYqE
  
 # Start the Java application
 java -jar /app/app.jar
